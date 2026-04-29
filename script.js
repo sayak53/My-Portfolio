81d999a6ef6d2421a7ae5c37c03e999a711ea084
sayak53 <sayakchakraborty928@gmail.com>
2026-04-29 21:30:36 +0530

    developed the about section and adjust some styles

---
+++ script.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll('.hidden-section');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show-section');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+console.log("hello");
